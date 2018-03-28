26bbdc30529175f9fcbde0ba6d93d18a48ae3bd7
Nadiia <chernikovahoups@gmail.com>
2018-03-28 10:00:16 +0200

    дополнение урок2

--- lesson1.2/script.js
+++ lesson1.2/script.js
@@ -1,4 +1,4 @@
-var num = 33721;
+/*var num = 33721;
 var i = num % 10;
 
 var i1 = Math.floor(num/10) % 10;
@@ -10,4 +10,30 @@ var multy = i*i1*i2*i3*i4;
 console.log(multy, "произведение (умножение) цифр " + num +" числа");
 
 var pow = multy**3;
-alert ("Возведение в 3 степень " + pow);
+alert ("Возведение в 3 степень " + pow);*/
+
+
+let x = 5; 
+//alert( x++ );
+console.log(x++)
+
+var arr=[];
+var string=arr + false - null + true 
+console.log(string)
+
+/*
+let y = 1; 
+let x = y = 2; 
+console.log(x);
+alert(x); 
+*/
+/*
+var arr=[];
+var string=arr + 1 + 2
+console.log(string)
+*/
+/*
+var two=2;
+var one=1;
+var string= two && one && null && undefined;
+console.log(string);*/
